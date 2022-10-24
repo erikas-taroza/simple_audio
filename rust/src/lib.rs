350afcbb40ac83fa014ca8fc07e17b54cc277af8
TypicalEgg <erikastaroza@gmail.com>
2022-10-24 01:02:15 -0700

    Work on opening file.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,32 +1,80 @@
+use symphonia::{
+    core::{
+        codecs::CodecRegistry, 
+        probe::Probe, 
+        io::{
+            MediaSource, MediaSourceStream
+        },
+        formats::FormatOptions,
+        meta::MetadataOptions
+    }, default
+};
+
 struct Player
 {
-
+    codec_registry:&'static CodecRegistry,
+    probe:&'static Probe,
+    format_options:FormatOptions,
+    metadata_options:MetadataOptions
+    // media_source:Box<dyn MediaSource>
 }
 
 impl Player
 {
+    pub fn new() -> Self
+    {
+        Player
+        {
+            codec_registry: default::get_codecs(),
+            probe: default::get_probe(),
+            format_options: FormatOptions { enable_gapless: true, ..Default::default() },
+            metadata_options: Default::default()
+        }
+    }
+
     /// Points to either a local file or an online file.
-    pub fn open(path:&str)
+    pub fn open(&mut self, path:&str) -> Result<(), std::io::Error>
+    {
+        if path.contains("http") { return Ok(()); }
+
+        let file = std::fs::File::open(path)?;
+        let source = Box::new(file);
+        let source_stream = MediaSourceStream::new(source, Default::default());
+
+        let probe_result = self.probe.format(
+            &mut symphonia::core::probe::Hint::new(),
+            source_stream,
+            &self.format_options,
+            &self.metadata_options
+        );
+
+        match probe_result
+        {
+            Err(err) => return Ok(()),
+            Ok(mut probed) => {
+                
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn play(&self)
     {
 
     }
 
-    pub fn play()
+    pub fn pause(&self)
     {
 
     }
 
-    pub fn pause()
+    pub fn seek(&self, seconds:i32)
     {
 
     }
 
-    pub fn seek(seconds:i32)
-    {
-
-    }
-
-    pub fn set_volume(volume:f32)
+    pub fn set_volume(&self, volume:f32)
     {
 
     }
