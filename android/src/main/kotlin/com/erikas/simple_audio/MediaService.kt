1173350c418a671205313184b53531d416b952ff
TypicalEgg <erikastaroza@gmail.com>
2022-11-16 13:08:15 -0800

    Work on android media notification.
    A connection of Dart to Kotlin was made.  The mediaSession has more capabilities now.

--- android/src/main/kotlin/com/erikas/simple_audio/MediaService.kt
+++ android/src/main/kotlin/com/erikas/simple_audio/MediaService.kt
@@ -1,10 +1,14 @@
 package com.erikas.simple_audio
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
+import android.os.Binder
 import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.MediaMetadataCompat.*
@@ -17,10 +21,10 @@ import androidx.media.MediaBrowserServiceCompat
 class MediaService : MediaBrowserServiceCompat()
 {
     private val channelId:String = "SimpleAudio::Notification"
+    private val notificationId:Int = 777
 
     private var mediaSession:MediaSessionCompat? = null
-    private lateinit var stateBuilder:PlaybackStateCompat.Builder
-    private lateinit var metadataBuilder:MediaMetadataCompat.Builder
+    private lateinit var playbackState:PlaybackStateCompat.Builder
 
     private val callback = object:MediaSessionCompat.Callback() {
         override fun onPlay() {
@@ -32,7 +36,20 @@ class MediaService : MediaBrowserServiceCompat()
             println("Pause")
             super.onPause()
         }
+
+        override fun onStop() {
+            stopSelf()
+            super.onStop()
+        }
     }
+
+    // This binder allows us to call instance methods to this service.
+    // Without this, calling MediaService().updateMediaSession will not work
+    // because the mediaSession won't be initialized.
+    inner class MediaServiceBinder:Binder()
+    { fun getService():MediaService = this@MediaService }
+
+    override fun onBind(intent: Intent?): IBinder? = MediaServiceBinder()
 
     override fun onGetRoot(
         clientPackageName: String,
@@ -49,44 +66,74 @@ class MediaService : MediaBrowserServiceCompat()
         result.sendResult(null)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCreate()
+    private fun getNotificationManager():NotificationManager
+    { return getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
+
+    private fun buildNotification():Notification
     {
-        super.onCreate()
-
-        mediaSession = MediaSessionCompat(baseContext, "SimpleAudio").apply {
-            stateBuilder = PlaybackStateCompat.Builder()
-                .setActions(PlaybackStateCompat.ACTION_PLAY or PlaybackStateCompat.ACTION_PAUSE)
-
-            metadataBuilder = MediaMetadataCompat.Builder()
-            metadataBuilder.putText(METADATA_KEY_TITLE, "Test")
-            metadataBuilder.putText(METADATA_KEY_ARTIST, "TEST TEST")
-            metadataBuilder.putLong(METADATA_KEY_DURATION, 100)
-
-            setPlaybackState(stateBuilder.build())
-            setMetadata(metadataBuilder.build())
-            setCallback(callback)
-            setSessionToken(sessionToken)
-        }
-
-        val channel = NotificationChannel(channelId, "SimpleAudio", NotificationManager.IMPORTANCE_HIGH)
-
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        manager.createNotificationChannel(channel)
-
-        val notification = NotificationCompat.Builder(baseContext, channelId).apply {
+        return NotificationCompat.Builder(baseContext, channelId).apply {
             val metadata = mediaSession!!.controller.metadata
             setContentTitle(metadata.getText(METADATA_KEY_TITLE))
             setContentText(metadata.getText(METADATA_KEY_ARTIST))
-            setContentIntent(mediaSession!!.controller.sessionActivity)
+            setSubText(metadata.getText(METADATA_KEY_ALBUM))
 
             // Required for showing the media style notification.
             setSmallIcon(R.mipmap.ic_launcher)
             setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             setStyle(androidx.media.app.NotificationCompat.MediaStyle()
                 .setMediaSession(mediaSession!!.sessionToken))
+        }.build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate()
+    {
+        super.onCreate()
+
+        mediaSession = MediaSessionCompat(baseContext, "SimpleAudio").apply {
+            playbackState = PlaybackStateCompat.Builder()
+                .setActions(
+                    PlaybackStateCompat.ACTION_PLAY
+                    or PlaybackStateCompat.ACTION_PAUSE
+                    or PlaybackStateCompat.ACTION_PLAY_PAUSE
+                    or PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+                    or PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                    or PlaybackStateCompat.ACTION_FAST_FORWARD
+                    or PlaybackStateCompat.ACTION_REWIND
+                    or PlaybackStateCompat.ACTION_SEEK_TO
+                )
+                .setState(PlaybackStateCompat.STATE_PLAYING, 0, 1.0f)
+
+            val metadataBuilder = MediaMetadataCompat.Builder()
+
+            setPlaybackState(playbackState.build())
+            setMetadata(metadataBuilder.build())
+            setCallback(callback)
+            setSessionToken(sessionToken)
         }
 
-        startForeground(777, notification.build())
+        val channel = NotificationChannel(channelId, "SimpleAudio", NotificationManager.IMPORTANCE_HIGH)
+        getNotificationManager().createNotificationChannel(channel)
+
+        startForeground(notificationId, buildNotification())
+    }
+
+    fun updateMediaSession()
+    {
+        playbackState.setState(PlaybackStateCompat.STATE_PLAYING, 50, 1.0f)
+
+        val metadataBuilder = MediaMetadataCompat.Builder().apply {
+            putText(METADATA_KEY_TITLE, "Title")
+            putText(METADATA_KEY_ARTIST, "Artist")
+            putText(METADATA_KEY_ALBUM, "Album")
+            putLong(METADATA_KEY_DURATION, 100)
+        }
+
+        mediaSession!!.apply {
+            setPlaybackState(playbackState.build())
+            setMetadata(metadataBuilder.build())
+        }
+
+        getNotificationManager().notify(notificationId, buildNotification())
     }
 }
--- android/src/main/kotlin/com/erikas/simple_audio/SimpleAudioPlugin.kt
+++ android/src/main/kotlin/com/erikas/simple_audio/SimpleAudioPlugin.kt
@@ -8,28 +8,35 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+// Initialized in the user's MainActivity.kt file.
+lateinit var mediaService:MediaService
+
 /** SimpleAudioPlugin */
-class SimpleAudioPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class SimpleAudioPlugin: FlutterPlugin, MethodCallHandler
+{
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel : MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "simple_audio")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding)
+    {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "simple_audio")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding)
+    {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result)
+    {
+        if(call.method == "updateMediaSession")
+        {
+            mediaService.updateMediaSession()
+        }
+        else { result.notImplemented() }
+    }
 }
