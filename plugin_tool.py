2a80534f40d2c51f40f508b616484aef5f9f2a9a
TypicalEgg <erikastaroza@gmail.com>
2022-12-24 02:43:09 -0800

    Fix yaml versions for publishing.

--- plugin_tool.py
+++ plugin_tool.py
@@ -58,7 +58,7 @@ def init():
             pubspec_text = add_dependency("ffi", version="^2.0.1", dev=False)
 
         if "flutter_rust_bridge:" not in pubspec_text:
-            pubspec_text = add_dependency("flutter_rust_bridge", version="^1.50.0", dev=False)
+            pubspec_text = add_dependency("flutter_rust_bridge", version="^1.57.0", dev=False)
         
         if "ffigen:" not in pubspec_text:
             pubspec_text = add_dependency("ffigen", version="^6.1.2", dev=True)
@@ -164,7 +164,7 @@ def init():
 def code_gen():
     print("Generating code with flutter_rust_bridge...\n")
 
-    os.system("cargo install flutter_rust_bridge_codegen")
+    os.system("cargo install flutter_rust_bridge_codegen --version 1.57.0")
     os.system('CPATH="$(clang -v 2>&1 | grep "Selected GCC installation" | rev | cut -d\' \' -f1 | rev)/include" \
         flutter_rust_bridge_codegen \
         --rust-input ./rust/src/lib.rs \
