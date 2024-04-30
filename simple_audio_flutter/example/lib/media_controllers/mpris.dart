13d2b7a21cb2abac3f72cd3c1b85825dea7e9ff6
Erikas Taroza <erikastaroza@gmail.com>
2024-04-30 11:30:04 -0700

    Add comments on raise function

--- simple_audio_flutter/example/lib/media_controllers/mpris.dart
+++ simple_audio_flutter/example/lib/media_controllers/mpris.dart
@@ -48,6 +48,31 @@ class Mpris extends MPRISService with mc.MediaController {
   Future<void> onSetPosition(String trackId, int position) =>
       player.seek(Duration(microseconds: position));
 
+  /*
+    In order for this to work, you need the following code in `my_application.cc` in the linux folder:
+    
+    static void my_application_activate(GApplication* application) {
+      GList* windows = gtk_application_get_windows(GTK_APPLICATION(application));
+      if(windows)
+      {
+        gtk_window_present_with_time(
+          GTK_WINDOW(windows->data),
+          g_get_monotonic_time() / 1000
+        );
+        return;
+      }
+      // ...
+    }
+
+
+    MyApplication* my_application_new() {
+      return MY_APPLICATION(g_object_new(my_application_get_type(),
+        "application-id", APPLICATION_ID, "flags",
+        G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN,
+        nullptr
+      ));
+    }
+  */
   @override
   Future<void> onRaise() async => Process.run(Platform.resolvedExecutable, []);
 
