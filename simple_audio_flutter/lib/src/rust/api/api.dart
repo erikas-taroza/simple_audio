d8c64a3d1bbb0df8901f0bf92b068d59d5edd189
Erikas Taroza <erikastaroza@gmail.com>
2025-01-04 02:59:14 -0800

    Exclude generated code from lint

--- simple_audio_flutter/lib/src/rust/api/api.dart
+++ simple_audio_flutter/lib/src/rust/api/api.dart
@@ -8,7 +8,7 @@ import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 import 'package:freezed_annotation/freezed_annotation.dart' hide protected;
 part 'api.freezed.dart';
 
-// These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `into`, `into`, `into`
+// These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `from`, `from`, `from`
 
 // Rust type: RustOpaqueMoi<Player>
 abstract class Player implements RustOpaqueInterface {}
