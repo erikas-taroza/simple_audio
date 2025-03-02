c4094966f4d92b07fbb5d8a056a4f5b7becd88ad
Erikas Taroza <erikastaroza@gmail.com>
2025-03-01 19:10:26 -0800

    Finish windows build command

--- cli/commands/build/subcommands/windows.dart
+++ cli/commands/build/subcommands/windows.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:io/io.dart';
 
 import '../../../cli_command.dart';
+import '../../../mixins/cli_logger.dart';
+import '../../../mixins/package_info.dart';
+import '../../../mixins/process_runner.dart';
 
-class WindowsBuildCommand extends CliCommand {
+class WindowsBuildCommand extends CliCommand
+    with CliLogger, ProcessRunner, PackageInfo {
   @override
   String get name => "windows";
 
@@ -11,6 +17,50 @@ class WindowsBuildCommand extends CliCommand {
 
   @override
   Future<int> run() async {
+    // Ensure rustup targets are installed.
+    logger.stdout("Installing rustup targets...");
+    int result = await runProcess(
+      "rustup",
+      [
+        "target",
+        "add",
+        "x86_64-pc-windows-msvc",
+      ],
+      logger: logger,
+    );
+
+    if (result != ExitCode.success.code) {
+      return result;
+    }
+
+    logger.stdout("Building Windows binary...");
+    result = await runProcess(
+      "cargo",
+      [
+        "build",
+        "--release",
+        "--target",
+        "x86_64-pc-windows-msvc",
+      ],
+      logger: logger,
+    );
+
+    if (result != ExitCode.success.code) {
+      return result;
+    }
+
+    final File file =
+        File("$projectRootDirectory/$packageName/windows/$packageName.dll");
+    if (await file.exists()) {
+      logger.trace("Found existing Windows binary. Deleting...");
+      await file.delete();
+    }
+
+    await File(
+      "$projectRootDirectory/target/x86_64-pc-windows-msvc/release/$packageName.dll",
+    ).rename("$projectRootDirectory/$packageName/windows/$packageName.dll");
+
+    logger.stdout("Done!");
     return ExitCode.success.code;
   }
 }
