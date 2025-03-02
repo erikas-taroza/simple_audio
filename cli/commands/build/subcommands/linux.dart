ea3d48896ff0a81cf5709cb2b1fadfa519fc80ec
Erikas Taroza <erikastaroza@gmail.com>
2025-03-01 19:01:43 -0800

    Finish Linux build command

--- cli/commands/build/subcommands/linux.dart
+++ cli/commands/build/subcommands/linux.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:io/io.dart';
 
 import '../../../cli_command.dart';
+import '../../../mixins/cli_logger.dart';
+import '../../../mixins/package_info.dart';
+import '../../../mixins/process_runner.dart';
 
-class LinuxBuildCommand extends CliCommand {
+class LinuxBuildCommand extends CliCommand
+    with CliLogger, ProcessRunner, PackageInfo {
   @override
   String get name => "linux";
 
@@ -11,6 +17,49 @@ class LinuxBuildCommand extends CliCommand {
 
   @override
   Future<int> run() async {
+    // Ensure rustup targets are installed.
+    logger.stdout("Installing rustup targets...");
+    int result = await runProcess(
+      "rustup",
+      [
+        "target",
+        "add",
+        "x86_64-unknown-linux-gnu",
+      ],
+      logger: logger,
+    );
+
+    if (result != ExitCode.success.code) {
+      return result;
+    }
+
+    logger.stdout("Building Linux binary...");
+    result = await runProcess(
+      "cargo",
+      [
+        "build",
+        "--release",
+        "--target",
+        "x86_64-unknown-linux-gnu",
+      ],
+      logger: logger,
+    );
+
+    if (result != ExitCode.success.code) {
+      return result;
+    }
+
+    final File file =
+        File("$projectRootDirectory/$packageName/linux/lib$packageName.so");
+    if (await file.exists()) {
+      logger.trace("Found existing Linux binary. Deleting...");
+      await file.delete();
+    }
+
+    File("$projectRootDirectory/target/x86_64-unknown-linux-gnu/release/lib$packageName.so")
+        .rename("$projectRootDirectory/$packageName/linux/lib$packageName.so");
+
+    logger.stdout("Done!");
     return ExitCode.success.code;
   }
 }
