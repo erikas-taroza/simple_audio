302f1c9dbe9a95463b660497ac23f0ad4321e0c5
Erikas Taroza <erikastaroza@gmail.com>
2025-03-01 22:05:47 -0800

    Fix CI

--- cli/main.dart
+++ cli/main.dart
@@ -3,10 +3,13 @@ import 'package:args/command_runner.dart';
 import 'commands/build/build.dart';
 import 'commands/update.dart';
 import 'commands/codegen.dart';
+import 'mixins/package_info.dart';
 
 void main(List<String> args) {
   final CommandRunner<int> runner =
       CommandRunner("package", "A tool to help with maintaining the package.");
+
+  PackageInfo.initProjectRootDirectory();
 
   runner
     ..addCommand(BuildCommand())
--- cli/mixins/package_info.dart
+++ cli/mixins/package_info.dart
@@ -5,8 +5,25 @@ import '../cli_command.dart';
 mixin PackageInfo on CliCommand {
   String get frbVersion => "2.7.0";
   String get packageName => "simple_audio_flutter";
-  String get projectRootDirectory {
-    final String current = Directory.current.path;
-    return "${current.split("simple_audio")[0]}simple_audio";
+
+  String get projectRootDirectory => _projectRootDirectory;
+  static String _projectRootDirectory = "";
+  static void initProjectRootDirectory() {
+    Uri path = Uri.parse(Directory.current.path);
+
+    // Starts from the current directory and goes up until it finds the root project directory.
+    while (true) {
+      List<String> items = Directory.fromUri(path)
+          .listSync()
+          .map((e) => e.uri.pathSegments.last)
+          .toList();
+
+      if (items.contains("pubspec.yaml") && items.contains("Cargo.toml")) {
+        _projectRootDirectory = path.toString();
+        break;
+      } else {
+        path = path.replace(pathSegments: path.pathSegments..removeLast());
+      }
+    }
   }
 }
